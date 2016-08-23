89631ffd7b79e4267d7c327867bf68bd0beb6c00
James Oliver <joliver@rancher.com>
2016-08-23 10:24:47 -0700

    distribute keys/certs via metadata

--- images/origin/run.sh
+++ images/origin/run.sh
@@ -1,4 +1,5 @@
-#!/bin/bash -x
+#!/bin/bash
+if [ "$RANCHER_DEBUG" == "true" ]; then set -x; fi
 
 oc_gate() {
   oc $@ &> /dev/null
@@ -9,28 +10,50 @@ oc_gate() {
 }
 
 common() {
-  HOST_IP=$(curl -s http://rancher-metadata/2015-12-19/self/host/agent_ip)
-  HOST_NAME=$(curl -s http://rancher-metadata/2015-12-19/self/host/hostname)
+  META_URL=http://rancher-metadata.rancher.internal/2015-12-19
+  HOST_IP=$(curl -s ${META_URL}/self/host/agent_ip)
+  # sometimes returns empty??
+  while [ "$HOST_IP" == "" ]; do
+    sleep 1
+    HOST_IP=$(curl -s ${META_URL}/self/host/agent_ip)
+  done
+
+  HOST_NAME=$(curl -s ${META_URL}/self/host/hostname)
   MASTER_CONFIG=/etc/origin/master
-  MASTER_CERT=${MASTER_CONFIG}/ca.crt
-  MASTER_KEY=${MASTER_CONFIG}/ca.key
-  MASTER_SERIAL=${MASTER_CONFIG}/ca.serial.txt
+  CA_CERT=${MASTER_CONFIG}/ca.crt
+  CA_KEY=${MASTER_CONFIG}/ca.key
+  CA_SERIAL=${MASTER_CONFIG}/ca.serial.txt
+  ADMIN_CFG=${MASTER_CONFIG}/admin.kubeconfig
   DOCKER_CERTS=/etc/docker/certs.d
 }
 
 bootstrap_master() {
   common
-  CREATE_EXAMPLES=${CREATE_EXAMPLES:-true}
-  CREATE_ROUTER=${CREATE_ROUTER:-true}
-  CREATE_REGISTRY=${CREATE_REGISTRY:-true}
 
   # Generate keys and default config
   openshift start master \
-    --etcd=http://etcd:2379 \
+    --etcd=http://etcd.rancher.internal:2379 \
     --master=https://${HOST_IP}:8443 \
     --public-master=https://${HOST_IP}:8443 \
     --cors-allowed-origins=localhost,127.0.0.1,${HOST_IP},${HOST_NAME},origin-master \
     --write-config=${MASTER_CONFIG}
+
+  UUID=$(curl -s ${META_URL}/services/origin-master/uuid)
+  SERVICE_DATA=$(curl -s -u $CATTLE_ACCESS_KEY:$CATTLE_SECRET_KEY "${CATTLE_URL}/services?uuid=$UUID")
+  PROJECT_ID=$(echo $SERVICE_DATA | jq -r '.data[0].accountId')
+  SERVICE_ID=$(echo $SERVICE_DATA | jq -r '.data[0].id')
+  SERVICE_DATA=$(curl -s -u $CATTLE_ACCESS_KEY:$CATTLE_SECRET_KEY "${CATTLE_URL}/projects/$PROJECT_ID/services/$SERVICE_ID")
+  SERVICE_DATA=$(echo $SERVICE_DATA | jq -r ".metadata |= .+ {\"ca.crt\":\"$(cat $CA_CERT | base64)\"}")
+  SERVICE_DATA=$(echo $SERVICE_DATA | jq -r ".metadata |= .+ {\"ca.key\":\"$(cat $CA_KEY | base64)\"}")
+  SERVICE_DATA=$(echo $SERVICE_DATA | jq -r ".metadata |= .+ {\"ca.serial\":\"$(cat $CA_SERIAL | base64)\"}")
+  SERVICE_DATA=$(echo $SERVICE_DATA | jq -r ".metadata |= .+ {\"admin.kubeconfig\":\"$(cat $ADMIN_CFG | base64)\"}")
+
+  curl -s -X PUT \
+    -u "${CATTLE_ACCESS_KEY}:${CATTLE_SECRET_KEY}" \
+    -H 'Accept: application/json' \
+    -H 'Content-Type: application/json' \
+    -d "${SERVICE_DATA}" \
+    "${CATTLE_URL}/projects/$PROJECT_ID/services/$SERVICE_ID"
 
   configure &
 
@@ -42,22 +65,29 @@ bootstrap_master() {
 
 bootstrap_node() {
   common
-  MASTER_IP=$(curl rancher-metadata/latest/services/origin-master/containers/0/primary_ip)
+  MASTER_IP=$(curl -s rancher-metadata/latest/services/origin-master/containers/0/primary_ip)
 
-  # TODO (llparse) make master keys available to nodes
+  mkdir -p ${MASTER_CONFIG}
+  UUID=$(curl -s ${META_URL}/services/origin-master/uuid)
+  SERVICE_DATA=$(curl -s -u $CATTLE_ACCESS_KEY:$CATTLE_SECRET_KEY "${CATTLE_URL}/services?uuid=$UUID")
+  echo $SERVICE_DATA | jq -r '.data[0].metadata."ca.crt"' | base64 -d > $CA_CERT
+  echo $SERVICE_DATA | jq -r '.data[0].metadata."ca.key"' | base64 -d > $CA_KEY
+  echo $SERVICE_DATA | jq -r '.data[0].metadata."ca.serial"' | base64 -d > $CA_SERIAL
+  echo $SERVICE_DATA | jq -r '.data[0].metadata."admin.kubeconfig"' | base64 -d > $ADMIN_CFG
+  # FIXME maybe we need more config?
+
   # Generate keys and default config
   oadm create-node-config \
-    --node-client-certificate-authority=${MASTER_CERT} \
-    --certificate-authority=${MASTER_CERT} \
-    --signer-cert=${MASTER_CERT} \
-    --signer-key=${MASTER_KEY} \
-    --signer-serial=${MASTER_SERIAL} \
+    --node-client-certificate-authority=${CA_CERT} \
+    --certificate-authority=${CA_CERT} \
+    --signer-cert=${CA_CERT} \
+    --signer-key=${CA_KEY} \
+    --signer-serial=${CA_SERIAL} \
     --node-dir=/etc/origin/node \
     --node=${HOST_NAME} \
     --hostnames=${HOST_IP} \
     --master=https://${MASTER_IP}:8443 \
     --config=/etc/origin/node
-
 
   openshift start node \
     --config=/etc/origin/node/node-config.yaml \
@@ -65,6 +95,10 @@ bootstrap_node() {
 }
 
 configure() {
+  CREATE_EXAMPLES=${CREATE_EXAMPLES:-true}
+  CREATE_ROUTER=${CREATE_ROUTER:-true}
+  CREATE_REGISTRY=${CREATE_REGISTRY:-true}
+
   # wait for server to open socket
   giddyup probe tcp://${HOST_IP}:8443 --loop --min 1s --max 16s --backoff 2
 
@@ -123,9 +157,9 @@ create_registry() {
   giddyup probe tcp://${REGISTRY_IP}:5000 --loop --min 1s --max 16s --backoff 2
 
   oadm ca create-server-cert \
-      --signer-cert=${MASTER_CERT} \
-      --signer-key=${MASTER_KEY} \
-      --signer-serial=${MASTER_SERIAL} \
+      --signer-cert=${CA_CERT} \
+      --signer-key=${CA_KEY} \
+      --signer-serial=${CA_SERIAL} \
       --hostnames="docker-registry.default.svc.cluster.local,${REGISTRY_IP}" \
       --cert=/etc/secrets/registry.crt \
       --key=/etc/secrets/registry.key
@@ -161,8 +195,8 @@ create_registry() {
   destdir_name=${DOCKER_CERTS}/docker-registry.default.svc.cluster.local:5000
 
   mkdir -p $destdir_addr $destdir_name
-  cp ${MASTER_CERT} $destdir_addr
-  cp ${MASTER_CERT} $destdir_name
+  cp ${CA_CERT} $destdir_addr
+  cp ${CA_CERT} $destdir_name
 }
 
 if [ $# -eq 0 ]; then
